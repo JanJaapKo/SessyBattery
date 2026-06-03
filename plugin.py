1469c252789e40a31e8c24ad647c02f8e86fb406
Jan-Jaap Kostelijk <44156144+JanJaapKo@users.noreply.github.com>
2026-06-03 22:18:41 +0200

    FIX GAS METER

--- plugin.py
+++ plugin.py
@@ -719,7 +719,7 @@ class SessyBatteryPlugin:
         if deviceId not in Devices or (self.p1VoltageSwellL3Unit not in Devices[deviceId].Units):
             Domoticz.Unit(Name=deviceId + ' - Voltage swell L3', Unit=self.p1VoltageSwellL3Unit, TypeName="General", Subtype=19, Used=0, DeviceID=deviceId).Create()
         if deviceId not in Devices or (self.p1GasMeterUnit not in Devices[deviceId].Units):
-            Domoticz.Unit(Name=deviceId + ' - Gas meter', Unit=self.p1GasMeterUnit, TypeName="P1 Smart Meter", Subtype=2, Used=1, DeviceID=deviceId).Create()
+            Domoticz.Unit(Name=deviceId + ' - Gas meter', Unit=self.p1GasMeterUnit, Type=251, Subtype=2, Used=1, DeviceID=deviceId).Create()
         if deviceId not in Devices or (self.p1GasMeterTimeUnit not in Devices[deviceId].Units):
             Domoticz.Unit(Name=deviceId + ' - Gas meter timestamp', Unit=self.p1GasMeterTimeUnit, TypeName="General", Subtype=19, Used=0, DeviceID=deviceId).Create()
 
